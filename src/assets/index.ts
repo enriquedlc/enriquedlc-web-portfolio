b03cc9996ccab07374ed45dc3eba3ec458cbce27
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-08-17 23:10:11 +0100

    :hammer: added the image exports

---
+++ src/assets/index.ts
@@ -0,0 +1,16 @@
+// TODO: fix imports
+
+import xmark from "./close/x-mark.png";
+import logo from "./mainLogo/E-logo.png";
+import menu from "./menu/menuCircle.png";
+
+// developer Areas
+import backend from "./developerAreas/backend.png";
+import mobile from "./developerAreas/mobile.png";
+import web from "./developerAreas/web.png";
+
+// experience icons
+import itopIcon from "./experience-icons/itopIcon.png";
+import salesianosIcon from "./experience-icons/salesianosIcon2.png";
+
+export { backend, itopIcon, logo, menu, mobile, salesianosIcon, web, xmark };
