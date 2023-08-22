fb9394438ee802ff530daedd3ba721b39742d6b9
Enrique De La Concepción <101868281+enriquedlc@users.noreply.github.com>
2023-08-22 01:05:44 +0100

    :hammer:

--- src/constants/projects.ts
+++ src/constants/projects.ts
@@ -19,7 +19,7 @@ export type Project = {
 
 export const projects: Project[] = [
 	{
-		name: "Car Rent",
+		name: "Soon...",
 		description:
 			"Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
 		tags: [
@@ -37,10 +37,10 @@ export const projects: Project[] = [
 			},
 		],
 		image: coming_soon_leaning_right,
-		source_code_link: "https://github.com/",
+		source_code_link: "https://github.com/enriquedlc",
 	},
 	{
-		name: "Job IT",
+		name: "Soon...",
 		description:
 			"Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
 		tags: [
@@ -58,10 +58,10 @@ export const projects: Project[] = [
 			},
 		],
 		image: coming_soon_straight,
-		source_code_link: "https://github.com/",
+		source_code_link: "https://github.com/enriquedlc",
 	},
 	{
-		name: "Trip Guide",
+		name: "Soon...",
 		description:
 			"A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
 		tags: [
@@ -79,6 +79,6 @@ export const projects: Project[] = [
 			},
 		],
 		image: coming_soon_leaning_left,
-		source_code_link: "https://github.com/",
+		source_code_link: "https://github.com/enriquedlc",
 	},
 ];
